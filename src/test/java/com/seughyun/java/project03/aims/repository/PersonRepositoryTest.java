26ebbc4b012d2d5eba22701dc287dff02b6216e0
bapadapa <painclown@gmail.com>
2021-02-17 17:20:28 +0900

    Spring AIMS 강의 시작

--- src/test/java/com/seughyun/java/project03/aims/repository/PersonRepositoryTest.java
+++ src/test/java/com/seughyun/java/project03/aims/repository/PersonRepositoryTest.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
-
+//test
 
 @SpringBootTest
 class PersonRepositoryTest  {
